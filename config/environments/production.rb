f72a2e5303538e7b3bae861d9fbcebdb241bad7d
OraCl13 <ostap.pelo@gotoinc.co>
2022-07-18 15:19:20 +0300

    auth edit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,9 @@ Rails.application.configure do
     :port      => ENV['MAILGUN_SMTP_PORT'],
     :user_name => 'ostapko2003o@gmail.com',
     :password  => ENV['MAILGUN_SMTP_PASSWORD'],
-    :authentication => ENV['MAILGUN_SMTP_LOGIN'],
+    :authentication => 'login',
     :domain => ENV['MAILGUN_DOMAIN'],
-    :enable_starttls_auto => true# ENV['MAILGUN_DOMAIN']
+    :enable_starttls_auto => true # ENV['MAILGUN_DOMAIN']
   }
 
   # Use a different logger for distributed setups.
