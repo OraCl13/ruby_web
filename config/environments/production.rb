500755d3e9ed1707c65436b2c6ba478c1184c00a
OraCl13 <ostap.pelo@gotoinc.co>
2022-07-18 16:48:41 +0300

    smtp again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,15 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'powerful-sands-05753.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :mailgun
-
-  config.action_mailer.mailgun_settings = { api_token: ENV['MAILGUN_API_TOKEN'], domain: 'powerful-sands-05753.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'powerful-sands-05753.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
